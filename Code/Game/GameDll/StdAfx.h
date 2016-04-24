e1ae7bcae507ddd829742603af3963ceba25d984
Matt McKeehan <matt31299@gmail.com>
2016-04-23 23:28:18 -0400

    Basic Code
    Basic Code

---
+++ Code/Game/GameDll/StdAfx.h
@@ -0,0 +1,18 @@
+// Copyright 2001-2016 Crytek GmbH / Crytek Group. All rights reserved.
+
+
+#pragma once
+
+
+#include <CryModuleDefs.h>
+#define eCryModule eCryM_Game
+#define GAME_API DLL_EXPORT
+
+#include <platform.h>
+#include <ISystem.h>
+#include <I3DEngine.h>
+#include <ISerialize.h>
+#include "player/extensions/ISimpleExtension.h"
+
+#include "FrameProfiler/FrameProfiler_JobSystem.h"	// to be removed
+
